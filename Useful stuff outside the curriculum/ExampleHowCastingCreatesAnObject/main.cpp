7436d3205d882fc26fd489091dff2d712a545e28
Martin <mapmoeu@gmail.com>
2018-08-05 01:10:14 +0300

    a simple example how casting creates a new object
    I encountered this case while testing my shared_ptr implementation and decided that it might be useful to save

---
+++ Useful stuff outside the curriculum/ExampleHowCastingCreatesAnObject/main.cpp
@@ -0,0 +1,9 @@
+
+#include "sharedPtr.h"
+
+int main()
+{
+	sharedPtr<int> a;
+	a = 5;
+	return 0;
+}
---
+++ Useful stuff outside the curriculum/ExampleHowCastingCreatesAnObject/sharedPtrContainer.h
@@ -0,0 +1,59 @@
+#pragma once
+template <class T>
+class sharedPtrContainer
+{
+public:
+	sharedPtrContainer();
+	sharedPtrContainer(const T&);
+	~sharedPtrContainer();
+
+	const T& operator *()const;
+	void UpdateCount(bool);
+
+	unsigned int GetCount()const;
+	T * GetDataAddress();
+private:
+	sharedPtrContainer(const sharedPtrContainer<T>&);
+	unsigned int m_Count;
+	T m_Data;
+};
+
+template<class T>
+inline sharedPtrContainer<T>::sharedPtrContainer() : m_Count(1), m_Data()
+{
+}
+
+template<class T>
+inline sharedPtrContainer<T>::sharedPtrContainer(const T & rhs) : m_Count(1), m_Data(rhs)
+{
+}
+
+template<class T>
+inline sharedPtrContainer<T>::~sharedPtrContainer()
+{
+	std::cout << "Data erased\n";
+}
+
+template<class T>
+inline const T & sharedPtrContainer<T>::operator*() const
+{
+	return this->m_Data;
+}
+
+template<class T>
+inline void sharedPtrContainer<T>::UpdateCount(bool increment)
+{
+	this->m_Count += (increment) ? 1 : -1;
+}
+
+template<class T>
+inline unsigned int sharedPtrContainer<T>::GetCount() const
+{
+	return this->m_Count;
+}
+
+template<class T>
+inline T * sharedPtrContainer<T>::GetDataAddress()
+{
+	return &(this->m_Data);
+}
