67a42c870bda0ea0849035dd753096e06ff7597b
Martin <mapmoeu@gmail.com>
2018-08-07 21:50:51 +0300

    removed some comments
    -removed some unnecessary comments

--- DataStructures/VectorAndVectorBasedClasses/Deque2.h
+++ DataStructures/VectorAndVectorBasedClasses/Deque2.h
@@ -7,8 +7,6 @@ class Deque2 : public Vector<T>
 public:
 	void pop_front();
 	void push_front(const T&);
-	//template <class T1>
-	//void friend swap(Deque2<T1>&, Deque2<T1>&);
 private:
 };
 
@@ -24,10 +22,3 @@ inline void Deque2<T>::push_front(const T & rhs)
 	Vector<T>::insert(0, rhs);
 }
 
-/*
-template<class T1>
-inline void swap(Deque2<T1>& lhs, Deque2<T1>& rhs)
-{
-	Vector<T1>::swap(lhs, rhs);
-}
-*/
