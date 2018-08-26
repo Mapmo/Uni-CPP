0878b9990250a27f3c2245499bf805d068aa9cb4
Martin <mapmoeu@gmail.com>
2018-08-26 22:11:19 +0300

    front added
    -added 2 overload functions for front() in Queue4

--- DataStructures/DequeAndDequeBasedClasses/Queue4.h
+++ DataStructures/DequeAndDequeBasedClasses/Queue4.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "Deque.h"
+
 template<class T>
-class Queue4<T> : protected Deque<T>
+class Queue4 : protected Deque<T>
 {
+	T& frontOverloadHelper();
+	T& backOverloadHelper();
 public:
 	//Element access
 
 	T & front();
 	const T& front()const;
-	T& back();
+	T & back();
 	const T& back()const;
 
 
@@ -26,8 +29,30 @@ public:
 
 	//Non - member functions
 
-	bool operator==(const Queue2<T>&);
-	bool operator!=(const Queue2<T>&);
 	template <class T1>
-	friend void swap(Queue2<T1>&, Queue2<T1>&);
+	friend bool operator==(const Queue4<T1>&, const Queue4<T1>&);
+
+	template <class T1>
+	friend bool operator!=(const Queue4<T1>&, const Queue4<T1>&);
+
+	template <class T1>
+	friend void swap(Queue4<T1>&, Queue4<T1>&);
 };
+
+template<class T>
+inline T & Queue4<T>::frontOverloadHelper()
+{
+	return Deque<T>::front();
+}
+
+template<class T>
+inline T & Queue4<T>::front()
+{
+	return frontOverloadHelper();
+}
+
+template<class T>
+inline const T & Queue4<T>::front() const
+{
+	return frontOverloadHelper();
+}
