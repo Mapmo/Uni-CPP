b80bde4fce7b329ef4e1c7c6e1226a45b52740d9
Martin <mapmoeu@gmail.com>
2018-08-28 22:03:04 +0300

    binary search tree updated
    -interface created
    -ctors added
    -cpy ctor and operator = set to private
    -GetFunctions added

--- DataStructures/Trees/BinarySearchTree.h
+++ DataStructures/Trees/BinarySearchTree.h
@@ -1,1 +1,49 @@
 #pragma once
+#include "..\Lists\LinkedLists\LinkedList2.h"
+
+template <class T>
+class BinarySearchTree
+{
+	BinarySearchTree(const BinarySearchTree<T>&);
+	BinarySearchTree<T>& operator=(const BinarySearchTree<T>&);
+public:
+	BinarySearchTree();
+
+	//ElementAccess
+	T& GetByKey(const int);
+	const T& GetByKey(const int) const;
+
+
+	//Modifiers
+
+	//if Erase() return false the binary search tree will know that the root was the last element
+	//left and it was destroyed and after that the tree is empty
+	bool Erase(const int);
+	void Insert(const int, const T&);
+private:
+	bool m_Empty;
+	LinkedList2<T> m_Branches;
+};
+
+template<class T>
+inline BinarySearchTree<T> & BinarySearchTree<T>::operator=(const BinarySearchTree<T>&)
+{
+	return *this;
+}
+
+template<class T>
+inline BinarySearchTree<T>::BinarySearchTree() : m_Empty(1), m_Branches()
+{
+}
+
+template<class T>
+inline T & BinarySearchTree<T>::GetByKey(const int numb)
+{
+	return m_Branches.GetByKey(numb);
+}
+
+template<class T>
+inline const T & BinarySearchTree<T>::GetByKey(const int numb) const
+{
+	return GetByKey(numb);
+}
