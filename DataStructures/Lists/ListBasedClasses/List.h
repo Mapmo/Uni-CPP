360d1d697004eeb7c509cd6591d7f86a8253640b
Martin <mapmoeu@gmail.com>
2018-08-29 13:24:56 +0300

    lists borders added
    -added m_Beg class memeber that points to the first eleemnt in the List
    -added m_End class memeber that points to the last eleemnt in the List

--- DataStructures/Lists/ListBasedClasses/List.h
+++ DataStructures/Lists/ListBasedClasses/List.h
@@ -9,17 +9,21 @@ public:
 	explicit List(LinkedList1<T, keyType>);
 	~List();
 private:
+	LinkedList1<T, keyType> * m_Beg;
+	LinkedList1<T, keyType> * m_End;
 	LinkedList1<T, keyType> * m_List;
 };
 
 template<class T, class keyType>
-inline List<T, keyType>::List() : m_List(nullptr)
+inline List<T, keyType>::List() : m_List(nullptr), m_Beg(nullptr), m_End(nullptr)
 {
 }
 
 template<class T, class keyType>
-inline List<T, keyType>::List(LinkedList1<T, keyType> rhs) : m_List(new LinkedList1<T, keyType> (rhs))
+inline List<T, keyType>::List(LinkedList1<T, keyType> rhs) : m_List(new LinkedList1<T, keyType>(rhs))
 {
+	this->m_Beg = m_List->begin();
+	this->m_End = m_List->end();
 }
 
 template<class T, class keyType>
