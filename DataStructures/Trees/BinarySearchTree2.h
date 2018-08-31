3cc2955c1175a8fecd9522acfafb3836f49d01d6
Martin <mapmoeu@gmail.com>
2018-08-31 20:23:55 +0300

    GetByKey() added
    -GetByKey() function added that finds and returns a leaf by its key
    -GetByKey has 2 overloads and an overload helper

--- DataStructures/Trees/BinarySearchTree2.h
+++ DataStructures/Trees/BinarySearchTree2.h
@@ -19,9 +19,19 @@ class BinarySearchTree2
 {
 	BinarySearchTree2(const BinarySearchTree2&) = delete;
 	const BinarySearchTree2<T>& operator=(const BinarySearchTree2&) = delete;
+
+	//OverloadHelpers
+	T& GetByKeyOverloadHelper(const int);
 public:
+	//Essentials
+
 	BinarySearchTree2(const int = 0, const T & = T());
 	virtual ~BinarySearchTree2();
+
+
+	//Element Access
+	T& GetByKey(const int);
+	const T& GetByKey(const int)const;
 private:
 	Branch<T> * m_Root;
 };
@@ -39,6 +49,35 @@ inline Branch<T>::~Branch()
 }
 
 template<class T>
+inline T & BinarySearchTree2<T>::GetByKeyOverloadHelper(const int numb)
+{
+	try
+	{
+		Branch<T> * tmp = this->m_Root;
+		while (tmp != nullptr)
+		{
+			if (tmp->key == numb)
+			{
+				return tmp->val;
+			}
+			else if (tmp->key > numb)
+			{
+				tmp = tmp->left;
+			}
+			else
+			{
+				tmp = tmp->right;
+			}
+		}
+		throw std::invalid_argument("no element with such a key found\n");
+	}
+	catch (std::invalid_argument& ia)
+	{
+		std::cerr << "GetByKey() threw an exception: " << ia.what();
+	}
+}
+
+template<class T>
 inline BinarySearchTree2<T>::BinarySearchTree2(const int k, const T & v) : m_Root(new Branch<T>(k, v))
 {
 }
@@ -47,4 +86,16 @@ template<class T>
 inline BinarySearchTree2<T>::~BinarySearchTree2()
 {
 	delete this->m_Root;
+}
+
+template<class T>
+inline T & BinarySearchTree2<T>::GetByKey(const int numb)
+{
+	return GetByKeyOverloadHelper(numb);
+}
+
+template<class T>
+inline const T & BinarySearchTree2<T>::GetByKey(const int numb) const
+{
+	return GetByKeyOverloadHelper(numb);
 }
