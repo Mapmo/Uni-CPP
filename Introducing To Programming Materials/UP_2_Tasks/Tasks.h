8df9f98f2df77cca205ad9cec5b46333ab07f808
Martin <mapmoeu@gmail.com>
2018-12-20 00:06:19 +0200

    Update Tasks.h

--- Introducing To Programming Materials/UP_2_Tasks/Tasks.h
+++ Introducing To Programming Materials/UP_2_Tasks/Tasks.h
@@ -1,44 +1,53 @@
 #pragma once
 int Task1(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the sum of the members of the array
 }
 
 int Task2(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the sum of the even members of the array
 }
 int Task3(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the biggest member of the array
 }
 int Task4(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the biggest uneven member of the array, return 0 if there are any
 }
 int Task5(const int arr[], const unsigned size)
 {
-//Return the smallest even member of the array, return 1 if there are any
+	return 0;
+	//Return the smallest even member of the array, return 1 if there are any
 }
 int Task6(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the sum of the even positive members of the array
 }
 int Task7(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return the biggest member with even index in the array
 }
 int Task8(const int arr[], const unsigned size)
 {
-//Return the sum of the members with uneven indexes in the array, return 0 if there is only 1 member
+	return 0;
+	//Return the sum of the members with uneven indexes in the array, return 0 if there is only 1 member
 }
 int Task9(const int arr[], const unsigned size)
 {
-	return arr[0] + arr[size - 1];
+	return 0;
 	//Return the sum of the first and last element in the array
 }
 bool Task10(const int arr[], const unsigned size)
 {
+	return 0;
 	//Return "True" if there are 2 equal adjacent members and "False" if there aren't
 }
 void Task11(int arr[], const unsigned size)
@@ -47,23 +56,27 @@ void Task11(int arr[], const unsigned size)
 }
 bool Task12(const int arr[], const unsigned size)
 {
-//return true if there are more even numbers in the array, else return false
+	return 0;
+	//return true if there are more even numbers in the array, else return false
 }
 bool Task13(int arr[], const unsigned size)
 {
+	return 0;
 	//Check whether the array is sorted in ascending order
 }
 bool Task14(int arr[], const unsigned size)
 {
+	return 0;
 	//Check whether the array is sorted in descending order
 }
 bool Task15(const int arr[], const unsigned size, const unsigned numb)
 {
-	return false;
+	return 0;
 	//Return "True" if numb is a member of the array or "False" if it isn't
 }
 unsigned Task16(const char arr[])
 {
+	return 0;
 	//implement strlen
 }
 void Task17(char arr[])
@@ -72,6 +85,7 @@ void Task17(char arr[])
 }
 bool Task18(const char arr[])
 {
+	return 0;
 	//Return "True" if the array contains U & P
 }
 void Task19(char arr[])
