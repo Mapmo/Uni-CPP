8b4575bfeb671da892ab341d7785223a9923a469
Martin <mapmoeu@gmail.com>
2019-05-26 03:01:58 +0300

    Update Test2.cpp

--- Tests/ISI/Test2.cpp
+++ Tests/ISI/Test2.cpp
@@ -139,7 +139,7 @@ int main()
 	q7.AddOption("Stepen na zrqlost (MATD)", -1);
 	exam.push_back(q7);
 
-	Question q8("Po kakvo se razlichavat modela na ranniq dizajn dizajn i post-arhitekturniq model na COCOMO II");
+	Question q8("Po kakvo se razlichavat modela na ranniq design i post-arhitekturniq model na COCOMO II");
 	q8.AddOption("Po vidovete prilojeniq obekt na ocenkata", -1);
 	q8.AddOption("Po slojnost na prilojeniqta", -1);
 	q8.AddOption("Po izpolzvanite faktori za razjod (multiplikatori na usiliq)", 1);
@@ -159,12 +159,12 @@ int main()
 	q10.AddOption("Procesi na obrabotka (PP)", -1);
 	q10.AddOption("Vynshni vhodove (EI)", 1);
 	exam.push_back(q10);
-
+	
 	Question q11("Koe tvyrdenie ne e vqrno za opredelqneto na granici na prilojenieto pri ocenka s metoda na funkcionalnite tochki?");
 	q11.AddOption("Granicite na prilojenieto se opredelqt ot potrebitelska gledna tochka", -1);
-	q11.AddOption("Granicite na prilojenieto trqbwa da se definirat maksimalno detajlno", -1);
+	q11.AddOption("Granicite na prilojenieto trqbva da se definirat maksimalno detajlno", -1);
 	q11.AddOption("Granicite na prilojenieto se menqt po vreme na ocenkata", 1);
-	q11.AddOption("Opredelenqneto na granicite na prilojenieto e zadyljitelno ori tozi model", -1);
+	q11.AddOption("Opredelenqneto na granicite na prilojenieto e zadyljitelno pri tozi model", -1);
 	exam.push_back(q11);
 
 	Question q12("Koq ot izbroenite definicii ne se otnasq za tranzakciqta za izvlichane na danni EQ");
@@ -202,13 +202,182 @@ int main()
 	q16.AddOption("Record Element Type", -1);
 	exam.push_back(q16);
 
+	Question q17("Tablica ot baza danni referirana ot prilojenieto, no upravlqvana ot drugo prilojenie se broi za:");
+	q17.AddOption("Vytreshen logicheski file", -1);
+	q17.AddOption("Vynshen interfeisen file", 1);
+	q17.AddOption("Data Element Type", -1);
+	q17.AddOption("Record Element Type", -1);
+	exam.push_back(q17);
+
+	Question q18("Koe tvyrdenie za Data Element Type ne e vqrno");
+	q18.AddOption("Poleta ot vytreshen logicheski file (ILF) vidimi za potrebitelq", -1);
+	q18.AddOption("Poleta ot vynshen interface file (EIF) vidimi za potrebitelq", -1);
+	q18.AddOption("Poleta, koito se izchislqvat avtomatichno", 1);
+	q18.AddOption("DET syotvestva na kolona v tablica na baza danni", -1);
+	exam.push_back(q18);
+
+	Question q19("Ot kakvo se opredelq slojnostta na funkciite za danni (EIF/ILF) pri analiza s funkcionalni tochki");
+	q19.AddOption("Ot broq na RET i DET", 1);
+	q19.AddOption("Ot kolichestviti informaciq izmerena v bytes", -1);
+	q19.AddOption("Poleta, koito se izchislqvat avtomatichno", -1);
+	q19.AddOption("DET syotvetstva na kolona v tablica na baza danni", -1);
+	exam.push_back(q19);
+
+	Question q20("Ot kakvo se opredelq slojnostta na funkciite za vynshen vhod (EI) pri analiza s funkcionalni tochki");
+	q20.AddOption("Ot broq na RET i DET", -1);
+	q20.AddOption("Ot broq na FTR", -1);
+	q20.AddOption("Ot broq na DET i FTR", -1);
+	q20.AddOption("Ot broq na DET", -1);
+	exam.push_back(q20);
+
+	Question q21("Kakyv e broqt na globalnite sistemni harakteristiki izpolzvani pri analiza s funckionalni tochki?");
+	q21.AddOption("7", -1);
+	q21.AddOption("17", -1);
+	q21.AddOption("21", -1);
+	q21.AddOption("14", 1);
+	exam.push_back(q21);
+
+	Question q22("Koe ot tvyrdeniq ne e vqrno za obektnite tochki");
+	q22.AddOption("Mqrka za razmer na funkcionalnost na softuer osnovavashta se na OOP", -1);
+	q22.AddOption("Obektnite tochki se osnovavat na metodologiite na strukturnoto programirane", 1);
+	q22.AddOption("Obektnite tochki sa tehnologichno orientirani", -1);
+	q22.AddOption("Pravilata za broene na obektnite tochki sa po-prosti v sravnenie s funckionalnite tochki", -1);
+	exam.push_back(q22);
+
+	Question q23("Ot kakvo se opredelq slojnostta na vytreshnite klasove pri analiz s obektni tochki");
+	q23.AddOption("Ot broq na RET i DET", 1);
+	q23.AddOption("Ot kolichestvoto informaciq syhranqvana v klasovete izmerena v bytes", -1);
+	q23.AddOption("Ot broq na metodite i uslugite raboteshti s vytreshnite klasove", -1);
+	q23.AddOption("Opredelq se po subektivna ocenka na ocenitelq", -1);
+	exam.push_back(q23);
+
+	Question q24("Koy elementaren proces ot analiza s funckionalni tochki syotvetstva na izhodna usluga pri analiza s obektni tochki");
+	q24.AddOption("Vytreshen logicheski file (ILF)", -1);
+	q24.AddOption("Vynshen interface file (EIF)", -1);
+	q24.AddOption("Vynshen vhod (EI)", -1);
+	q24.AddOption("Vynshen izhod (EO)", 1);
+	exam.push_back(q24);
+
+	Question q25("Koy element na analiza po metodologiqta SEER-SEM nqma analog pri analiza s funkcionalni tochki");
+	q25.AddOption("SEER-FBS vytreshni logicheski files (ILF)", -1);
+	q25.AddOption("SEER-FBS Vynshni interface files (EIF)", -1);
+	q25.AddOption("SEER-FBS Vytreshni funkcii (IF)", 1);
+	q25.AddOption("SEER-FBS Izvlichane na danni (EQ)", -1);
+	exam.push_back(q25);
+	
+	Question w1("Tablica ot baza danni upravlqvana ot prilojenieto se broi za");
+	w1.AddOption("Vytreshen vhod (EI)", -1);
+	w1.AddOption("Data Element Type", -1);
+	w1.AddOption("Record Element Type", -1);
+	w1.AddOption("vytreshen logicheski file (ILF)", 1);
+	exam.push_back(w1);
+
+	Question w2("Koe ot izbroenite pravila za identifikaciq ne se otnasq za external interface files (EIF)");
+	w2.AddOption("Grupata ot danni ili upravlqvashta informaciq e logicheska i vidima", -1);
+	w2.AddOption("Grupata ot danni e upravlqvana kato EIF v drugo prilojenie", 1);
+	w2.AddOption("Grupata ot danni e referirana i vynshna za prilojenieto", -1);
+	w2.AddOption("Grupata ot danni e upravlqvana izvyn ramkite na prilojenieto", -1);
+	exam.push_back(w2);
+
+	Question w3("Koga e umestno da se izpolzva modela na kompoziciq na prilojenieto pri COCOMO II");
+	w3.AddOption("Sled kato se opredeli arhitekturata na softuernoto prilojenie", -1);
+	w3.AddOption("Predi da se opredelqt iziskvaniqta na klienta", -1);
+	w3.AddOption("V nachalen etap na proekta ili pri izgotvqne na prototip", 1);
+	w3.AddOption("Sled izgotvqne na detaylen tehnicheski dizain na prilojenieto", -1);
+	exam.push_back(w3);
+
+	Question w7("Kolko sa faktorite za razhod v modela na ranniq design na COCOMO II");
+	w7.AddOption("14", -1);
+	w7.AddOption("17", -1);
+	w7.AddOption("7", 1);
+	w7.AddOption("5", -1);
+	exam.push_back(w7);
+
+	Question w9("Koe tvyrdenie ne e vqrno za opredelqneto na granici na prilojenieto s metoda na funkcionalnite tochki");
+	w9.AddOption("Granicite na prilojenieto trqbva da se definirat maksimalno detaylno", -1);
+	w9.AddOption("Granicite na prilojenieto ne se menqt po vreme na ocenka", -1);
+	w9.AddOption("Granicite na prilojenieto se opredelqt ot gledna tochka na arhitekta na sistemata", 1);
+	w9.AddOption("Opredelqneto na granicite na prilojenieto e zadylvitelno pri ocenkata na sistemata", -1);
+	exam.push_back(w9);
+
+	Question w14("Koe tvyrdenie za Data Element Types (DET) e vqrno");
+	w14.AddOption("Poleta ot vytreshen logicheski file (ILF), vidimi i znachimi za potrebitelq", 1);
+	w14.AddOption("Poleta ot vynshen interface file (EIF) nevidimi i neznachimi za potrebitelq", -1);
+	w14.AddOption("Poleta ot vytreshen logicheski file (ILF), nividimi i neznachimi za potrebitelq", -1);
+	w14.AddOption("DET syotvetstva na syhranena procedura(stored procedure)", -1);
+	exam.push_back(w14);
+
+	Question w15("Ot kakvo se opredelq slojnostta na funkciite za vynshen izhod (EO) pri analiza s funckionalni tochki");
+	w15.AddOption("Ot broq DET i FTR", 1);
+	w15.AddOption("Ot broq na RET i DET", -1);
+	w15.AddOption("Ot broq na FTR", -1);
+	w15.AddOption("Ot broq na RET", -1);
+	exam.push_back(w15);
+
+	Question w16("Pri opredelena edna i syshta slojnost na funkcionalnosti ot tip EI i EO koq bi poluchila po visoka ocenka");
+	w16.AddOption("EO", 1);
+	w16.AddOption("EI", -1);
+	w16.AddOption("Ravna ocenka", -1);
+	w16.AddOption("Subektivno e", -1);
+	exam.push_back(w16);
+
+	Question w18("Koe tvyrdenie e vqrno za funkcionalnite tochki?");
+	w18.AddOption("Mqrka za razmer na funkcionalnost na softuer osnovavashta se na OOP pogled", -1);
+	w18.AddOption("Funckionalnite tochki se osnovavat na metodologiite na strukturnoto programirane", -1);
+	w18.AddOption("Merna edinica za funkcionalnost, kato tezultata e predstaven v terminite na krajniq potrebitel", 1);
+	w18.AddOption("Ne se ocenqva s funckionalni tochki", -1);
+	exam.push_back(w18);
+
+	Question w19("Pri edna i syshta slojnost na funkciite za danni (ILF, EIF) ??? funkcionalnite tochki");
+	w19.AddOption("EIF se ocenqva s povech funckionalni tochki ot ILF", -1);
+	w19.AddOption("Broqt na funkcionalni tochki e ednakyv", -1);
+	w19.AddOption("ILF se ocenqva s povech funckionalni tochki ot EIF", 1);
+	w19.AddOption("Ne se ocenqvat s funkcionalni tochki", -1);
+	exam.push_back(w19);
+
+	Question w21("Za izchislqvane na razmera na softuer na osnovata na Use-Case Object Point e neobhodimo:");
+	w21.AddOption("Da se preborqt liniite kod bez komentari v prilojenieto", -1);
+	w21.AddOption("Da se opredelqt broq obektni tochki na prilojenieto", -1);
+	w21.AddOption("Da se identificirat faktorite za razhod v prilojenieto", -1);
+	w21.AddOption("Da se identificirat aktyorite", 1);
+	exam.push_back(w21);
+
+	Question w22("Koy ot izbroenite elementarni procesi ne e element na analiza s funkcionalni tochki");
+	w22.AddOption("Vynshni izhodi (EO)", -1);
+	w22.AddOption("Vytreshni interface files (IF)", 1);
+	w22.AddOption("Izvlichane na danni (EQ)", -1);
+	w22.AddOption("Vynshni vhodove (EI)", -1);
+	exam.push_back(w22);
+
+	Question w23("Buton vyv forma ot prilojenieto se broi za");
+	w23.AddOption("Vytreshen logicheski file (ILF)", -1);
+	w23.AddOption("Data Element Type (DET)", -1);
+	w23.AddOption("Record Element Type (RET)", -1);
+	w23.AddOption("Vynshen vhod (EI)", 1);
+	exam.push_back(w23);
+
+	Question w24("Koy model e chast ot COCOMO II metod za ocenka?");
+	w24.AddOption("Model na biznes procesite", -1);
+	w24.AddOption("Model na ranniq design", 1);
+	w24.AddOption("Use-case model", -1);
+	w24.AddOption("Model na bazite danni", -1);
+	exam.push_back(w24);
+
+	Question w25("V kakva posledovatelnost sledva da se izvyrshva analiza s funkcionalni tochki?");
+	w25.AddOption("opredelqne na faktori na slojnost, identifikaciq na funkcii, opredelqne na niva na slojnost, iz??? nekorigiran funkcionalen obem, opredelqne na faktori za korekciq na funkcionalen obem, opredelqne na funkcionalen obem", -1);
+	w25.AddOption("opredelqne na niva na slojnost, identifikaciq na funkcii, opredelqne na faktori na slojnost, iz??? nekorigiran funkcionalen obem, opredelqne na faktori za korekciq na funkcionalen obem, opredelqne na funkcionalen obem", -1);
+	w25.AddOption("identifikaciq na strukturi ot danni, opredelqne na faktori na slojnost, opredelqne na niva na slojnost, iz??? nekorigiran funkcionalen obem, opredelqne na faktori za korekciq na funkcionalen obem, opredelqne na funkcionalen obem", -1);
+	w25.AddOption("identifikaciq na funkcii, opredelqne na faktori na slojnost, opredelqne na niva na slojnost, iz??? nekorigiran funkcionalen obem, opredelqne na faktori za korekciq na funkcionalen obem, opredelqne na funkcionalen obem", 1);
+	exam.push_back(w25);
+
+	//opredelqne na funkcionalen obem
 	/*
-	Question q1("");
-	q1.AddOption("", -1);
-	q1.AddOption("", -1);
-	q1.AddOption("", -1);
-	q1.AddOption("", -1);
-	exam.push_back(q1);
+	Question w2("");
+	w2.AddOption("", -1);
+	w2.AddOption("", -1);
+	w2.AddOption("", -1);
+	w2.AddOption("", -1);
+	exam.push_back(w2);
 	*/
 
 	unsigned questionsCount = exam.size();
