48bff578718e51c75649f14eea2e4386ac971903
Martin <mapmoeu@gmail.com>
2019-06-26 01:49:22 +0300

    Update Test1.cpp

--- Tests/SARS/Test1.cpp
+++ Tests/SARS/Test1.cpp
@@ -98,25 +98,25 @@ int main()
 
 	Question r33("Arhitektirniqt stil Circuit Breaker\nIzberete Edno");
 	r33.AddOption("Znachitelno podobrqva izmenqemostta na sistemata", -1);
-	r33.AddOption("Podobrqva samo nadejdnostta na sistemata", -1);
+	r33.AddOption("Podobrqva samo nadejdnostta na sistemata", 1);
 	r33.AddOption("Podobrqva nadejdnostta i proizvoditelnostta na sistemata", -1);
 	r33.AddOption("Namalqva znachitelno izmenqemostta na sistemata", -1);
 	exam.push_back(r33);
 
 	Question r32("Shablonyt fasada se izpolzva pri neobhodimost ot uvelichavane na byrzodeystvieto na programata");
 	r32.AddOption("True", -1);
-	r32.AddOption("False", -1);
+	r32.AddOption("False", 1);
 	exam.push_back(r32);
 
 	Question r31("Koe ot izbroenite ne vliqe vurhu izbora na arhitekturni taktiki\nIzberete edno");
 	r31.AddOption("Bydeshti planove za razvitie na organizaciqta", -1);
 	r31.AddOption("Broqt i kvalifikaciqta na nalichnite razrabotchici", -1);
-	r31.AddOption("Kvalifikaciqta na manager-a na proekta", -1);
+	r31.AddOption("Kvalifikaciqta na manager-a na proekta", 1);
 	r31.AddOption("Nalichie na legacy software", -1);
 	exam.push_back(r31);
 
 	Question r30("V stilyt neqvno izvikvane (implicit invocation) komponentite vzaimodejstvat pomejdu si chrez:\nIzberete edno");
-	r30.AddOption("Izprashtane na t.nar sybitiq, pri koito izprashtashtiq komponent ne znae nishto za poluchatelq", -1);
+	r30.AddOption("Izprashtane na t.nar sybitiq, pri koito izprashtashtiq komponent ne znae nishto za poluchatelq", 1);
 	r30.AddOption("Izvikvane na metod, pri koeto izprashtashtiq komponent izchakva za otgovor, predi da prodylji sys svoeto izpylnenie", -1);
 	r30.AddOption("Izprashtane na t.nar sybitiq, pri koito izprashtashtiq komponent ima informaciq za poluchatelq", -1);
 	exam.push_back(r30);
@@ -136,9 +136,9 @@ int main()
 	exam.push_back(r28);
 
 	Question r27("Broqt na strukturite, koito shte bydat vklucheni v dokumentaciqta na softuernata arhitektura zavisi ot\nIzberete edno ili poveche");
-	r27.AddOption("Vidyt na zainteresovanite lica", -1);
+	r27.AddOption("Vidyt na zainteresovanite lica", 1);
 	r27.AddOption("Softuertnata sistema", -1);
-	r27.AddOption("Prilojnata oblast, kydeto shte se vnedrqva softuernata sistema", -1);
+	r27.AddOption("Prilojnata oblast, kydeto shte se vnedrqva softuernata sistema", 1);
 	r27.AddOption("Kodyt na sistemata", -1);
 	exam.push_back(r27);
 
@@ -173,8 +173,8 @@ int main()
 	r22.AddOption("Syvkupnost ot predstavqne na sistemata ot razlichni gledni tochki", -1);
 	exam.push_back(r22);
 
-	Question r21("V stila clien t-server komponentite vzaimodeystvat pomejdu si chrez izprashtane na syobshteniq(ili izvikvane na metod) pri koeto izprashtashtiq komponent ne znae nishto za poluchatelq?\nIzberete edno");
-	r21.AddOption("True", -1);
+	Question r21("V stila client-server komponentite vzaimodeystvat pomejdu si chrez izprashtane na syobshteniq(ili izvikvane na metod) pri koeto izprashtashtiq komponent ne znae nishto za poluchatelq?\nIzberete edno");
+	r21.AddOption("True", 1);
 	r21.AddOption("False", -1);
 	exam.push_back(r21);
 
@@ -210,15 +210,15 @@ int main()
 	r16.AddOption("Vkluchva opisanie na strukturite na arhitekturata plus pridrujavashti dokumenti", 1);
 	exam.push_back(r16);
 
-	Question q1("Koi ot slednite sa primer sa dobre formuliran scenarii za kachestvo? 1 ili poweche");
-	q1.AddOption("Dopustimo e sistemata da e w revim na poddrujka za ne poweche ot 3chasa sedmichno", 1);
-	q1.AddOption("Potrebitelqt wliza w sistemata, izbira opciqta za tursene I vuvejda operedelen simwolen niz", -1);
-	q1.AddOption("Promeni w sistemata da se prawqt wyzmovno nai-byrzo", -1);
-	q1.AddOption("Otgoworut na potrebitelskite zaqvki trqbva da stane do 30 sec", 1);
+	Question q1("Koi ot slednite sa primer sa dobre formuliran scenarii za kachestvo?\nIzberete edno 1 ili poveche");
+	q1.AddOption("Dopustimo e sistemata da e v rejim na poddrujka za ne poveche ot 3 chasa sedmichno", 1);
+	q1.AddOption("Potrebitelqt vliza v sistemata, izbira opciqta za tursene I vuvejda operedelen simvolen niz", -1);
+	q1.AddOption("Promeni v sistemata da se pravqt vyzmovno nai-byrzo", -1);
+	q1.AddOption("Otgovoryt na potrebitelskite zaqvki trqbva da stane do 30 sec", 1);
 	exam.push_back(q1);
 
-	Question q2("Smisulut na nefykcionalnite iziskvaniq kum sistemata e");
-	q2.AddOption("Da se ylesni razrabotkata na sistemata", -1);
+	Question q2("Smisulut na nefukcionalnite iziskvaniq kum sistemata e\nIzberete edno");
+	q2.AddOption("Da se ulesni razrabotkata na sistemata", -1);
 	q2.AddOption("Da se izpolzvat nai-suvremennite tehnologii I programni ezici", -1);
 	q2.AddOption("Da se namali cenata na razrabotka na softuerna sistema", -1);
 	q2.AddOption("Da se ytochni po kakuv nachin I pri kakvi ysloviq shte raboti sistemata", 1);
@@ -229,14 +229,14 @@ int main()
 	q3.AddOption("Luja", 1);
 	exam.push_back(q3);
 
-	Question q4("Pod proizvoditelnost v softuernite sistemi se rabira");
-	q4.AddOption("Vuzmojnost na sistemata da osigyri rabota bez otkaz", -1);
+	Question q4("Pod proizvoditelnost v softuernite sistemi se rabira\nIzberete edno");
+	q4.AddOption("Vuzmojnost na sistemata da osiguri rabota bez otkaz", -1);
 	q4.AddOption("Vremeto, za koeto sistemata reagira na vuznikvashti subitiq", 1);
 	q4.AddOption("Mqrka za sebestoinostta na promenite v sistemata", -1);
 	q4.AddOption("Vuzmojnost na potrebitql burzo da ovladee sistemata", -1);
 	exam.push_back(q4);
 
-	Question q5("V koi ot slednite redove e dopusnata greshka?");
+	Question q5("V koi ot slednite redove e dopusnata greshka?\nIzberete edno");
 	q5.AddOption("Vuzdeistvie, algorithm, iztochnik sa elementi na scenarii na kachestvo", 1);
 	q5.AddOption("Vuzdeistvie, kolichestveni parametri, kontekst sa elementi na scenarii na kachestvo", -1);
 	q5.AddOption("Vuzdeistvie, rezultat, iztochnik sa elementi na scenarii na kachestvo", -1);
@@ -248,59 +248,59 @@ int main()
 	q6.AddOption("Luja", -1);
 	exam.push_back(q6);
 
-	Question q7("Cost Benefit Analysis Method(CBAM) e proces pri koito:");
+	Question q7("Cost Benefit Analysis Method(CBAM) e proces pri koito:\nIzberete edno");
 	q7.AddOption("se opredelqt nai-vajnite iziskvaniq kum dokymentaciqta na arhitekturata", -1);
 	q7.AddOption("nito edno ot izbroenite ne e vqrno", 1);
 	q7.AddOption("se ocenqva proizvoditelnostta na sistemata", -1);
-	q7.AddOption("se proverqva dali proektiranta veche architektyra udovletvorqva postavenite iziskvaniq", -1);
+	q7.AddOption("se proverqva dali proektiranta veche architektura udovletvorqva postavenite iziskvaniq", -1);
 	exam.push_back(q7);
 
-	Question q8("Pri kakuv tip sistemi e nai-podhodqshto da se izpolzva stryktyrata na vnedrqvaneto");
-	q8.AddOption("Koito se izpylnqwat na edna mashina", -1);
+	Question q8("Pri kakuv tip sistemi e nai-podhodqshto da se izpolzva stryktyrata na vnedrqvaneto\nIzberete edno");
+	q8.AddOption("Koito se izpylnqvat na edna mashina", -1);
 	q8.AddOption("Sistemi s mnogo potrebiteli", -1);
-	q8.AddOption("Nito edin ot posochenite", -1);
+	q8.AddOption("Nito edin ot posochenite", 1);
 	q8.AddOption("Koito se izpulnqvat v cloud", -1);
 	exam.push_back(q8);
 
-	Question q9("Architektyrniq stil predstavlqva");
+	Question q9("Architektyrniq stil predstavlqva\nIzberete edno");
 	q9.AddOption("Obektno oprientiran model na softuernata sistema", -1);
 	q9.AddOption("Opisanie na softuernata arhitektura", -1);
 	q9.AddOption("Tipovo arhitekturno reshenie, koeto se dokazalo pri upotreba v konkretni sityacii", 1);
 	q9.AddOption("Model za prehod ot softuerna arhitektyra kym kod", -1);
 	exam.push_back(q9);
 
-	Question q10("Pri proektiraneto na arhitektyrata moje da se ogranichi borq na modyli, s koito daden komponent komunikira, za da se :");
-	q10.AddOption("ogranichi neobhodimostta ot promeni s drugi moduli, s koito dadeniqt komynikira", 1);
+	Question q10("Pri proektiraneto na arhitektyrata moje da se ogranichi borq na modyli, s koito daden komponent komunikira, za da se:\nIzberete edno");
+	q10.AddOption("ogranichi neobhodimostta ot promeni s drugi moduli, s koito dadeniqt komunikira", 1);
 	q10.AddOption("namali slovnostta na sistemata I da se yskori procesa na razrabotka", -1);
-	q10.AddOption("naprawi arhitekturata po-qsna I da se razbira ot wsichki zainteresowani lica", -1);
-	q10.AddOption("ne se prilaga takawa taktika", -1);
+	q10.AddOption("napravi arhitekturata po-qsna i da se razbira ot vsichki zainteresovani lica", -1);
+	q10.AddOption("ne se prilaga takava taktika", -1);
 	exam.push_back(q10);
 
-	Question q11("Stuktura na procesite :");
-	q11.AddOption("Posochwa zaduljeniqta na otdelnite zainteresowani lica", -1);
+	Question q11("Stuktura na procesite:");
+	q11.AddOption("Posochva zadyljeniqta na otdelnite zainteresovani lica", -1);
 	q11.AddOption("Pokazwa posledovatelnostta na izpulnenie na otdelnite komponenti v sistemata", 1);
-	q11.AddOption("E izgotwen ot menidjura na proekta dokument, koito naznachawa zadachite na ostanlite ychastnici w razrabotkata na proekta", -1);
-	q11.AddOption("Pokazwa abstraktni grupi ot hora, koito realizirat modulite na sistemata", -1);
+	q11.AddOption("E izgotven ot manager-a na proekta dokument, koito naznachava zadachite na ostanlite uchastnici v razrabotkata na proekta", -1);
+	q11.AddOption("Pokazva abstraktni grupi ot hora, koito realizirat modulite na sistemata", -1);
 	exam.push_back(q11);
 
-	Question q12("Koe ot slednite ne e vqrno za arhitektyren stil Pipe-Filter :");
+	Question q12("Koe ot slednite ne e vqrno za arhitektyren stil Pipe-Filter:\nIzberete edno");
 	q12.AddOption("Ima trudnosti pri realizaciqta na interaktivni prilojeniq", -1);
 	q12.AddOption("Wodi do prosti za proektirane I realizaciq arhitekturi", -1);
 	q12.AddOption("Nqma vyzmojnost za paralelna obrabotka na informaciqta", 1);
 	q12.AddOption("Strukturata mu e identichna sys strukturata na powecheto standartni biznes procesi w organizaciite", -1);
 	exam.push_back(q12);
 
-	Question q13("W softuernata arhitektura e redno da se opiswat swoistwa I metodi na klasowete, koito izgrajdat obektite w sistemata");
+	Question q13("V softuernata arhitektura e redno da se opiswat swoistwa I metodi na klasowete, koito izgrajdat obektite w sistemata");
 	q13.AddOption("True", -1);
 	q13.AddOption("False", 1);
 	exam.push_back(q13);
 
-	Question q14("V koi ot slednite deinosti e redno da wzimat ychastie softuernite arhitekti ? Edno ili poweche");
+	Question q14("V koi ot slednite deinosti e redno da vzimat uchastie softuernite arhitekti?\nIzberete edno ili poweche");
 	q14.AddOption("Opredelqne na algoritmite za razabotka na sistemata", -1);
 	q14.AddOption("Razrabotka na sistemata", 1);
-	q14.AddOption("Ytochnqwane na iziskwaniqta kym softuernata sistema", 1);
-	q14.AddOption("Proektirane na arhitektyrata", 1);
-	q14.AddOption("Izgotwqne na biznes plan na proekta", -1);
+	q14.AddOption("Utochnqvane na iziskvaniqta kym softuernata sistema", 1);
+	q14.AddOption("Proektirane na arhitekturata", 1);
+	q14.AddOption("Izgotvqne na biznes plan na proekta", 1);
 	exam.push_back(q14);
 
 	/*
