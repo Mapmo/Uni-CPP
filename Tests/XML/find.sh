c2505723988de2bc43f7bd72fb986c547bacb396
Martin <mapmoeu@gmail.com>
2019-11-18 00:44:44 +0200

    imrpoved search tool

--- Tests/XML/find.sh
+++ Tests/XML/find.sh
@@ -5,12 +5,19 @@ function get()
 	egrep "$1" "test.cpp" | egrep "[^-][[:digit:]]);" | head -1 | egrep -o "\".*\", ?[[:digit:].]*);$" | sed 's/,[^,]*$//'
 }
 
+YELLOW='\033[1;33m'
+NC='\033[0m'
+PINK='\033[1;35m'
+
+echo
+
 while read a
 do
 
-	echo -n "$a ::: " #separator
-	get $(echo $a | egrep -o "^[^(]*" | egrep -o "[q|w][[:digit:]]*$" ) #add dakata's letter to the last egrep regex
+	echo -en "$a ${YELLOW}<-->${NC} " #separator
+	echo -e "${PINK}$(get $(echo $a | egrep -o "^[^(]*" | egrep -o "[q|w][[:digit:]]*$" ))${NC}"  #add dakata's letter to the last egrep regex
+	echo
 
-done< <(egrep --color=auto -i ".*Question.*$1" "test.cpp")
+done< <(egrep -i ".*Question.*$1" "test.cpp")
 
-
+echo
