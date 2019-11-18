a6929f04264b59dc100c1f60698e44bdca43175d
Martin <mapmoeu@gmail.com>
2019-11-18 23:13:58 +0200

    fixed a bug caused by last update

--- Tests/find.sh
+++ Tests/find.sh
@@ -2,7 +2,7 @@
 
 function get()
 {
-	egrep "$@" "test.cpp" | egrep "[^-][[:digit:]]);" | head -1 | egrep -o "\".*\", ?[[:digit:].]*);$" | sed 's/,[^,]*$//'
+	egrep "$1" "test.cpp" | egrep "[^-][[:digit:]]);" | head -1 | egrep -o "\".*\", ?[[:digit:].]*);$" | sed 's/,[^,]*$//'
 }
 
 YELLOW='\033[1;33m'
@@ -18,6 +18,6 @@ do
 	echo -e "${PINK}$(get $(echo $a | egrep -o "^[^(]*" | egrep -o "[q|w][[:digit:]]*$" ))${NC}"  #add dakata's letter to the last egrep regex
 	echo
 
-done< <(egrep -i ".*Question.*$1" "test.cpp")
+done< <(egrep -i ".*Question.*$*" "test.cpp")
 
 echo
